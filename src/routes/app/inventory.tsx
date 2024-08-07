6618632a009e4ad61c3b9c570ee2581272f776a2
Decimal <DecimalCactus@gmail.com>
2024-08-06 20:50:59 -0400

    add get all inventory items api request and display information on the inventory page

--- src/routes/app/inventory.tsx
+++ src/routes/app/inventory.tsx
@@ -1,8 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router"; 
+import { Spinner } from "@/components/ui/shadcn/spinner";
+import { useGetAllInventoryItems } from "@/features/auth/api/get-inventory-items";
+import useUserStore from "@/hooks/useUserStore";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/app/inventory")({
+  beforeLoad: ({ context }: any) => {
+    if (!context.isAuthenticated) {
+      throw redirect({
+        to: "/",
+      });
+    }
+  },
   component: Inventory,
-});
+})
 
 /**
  * Make a "get-inventory-items" api hook to call the GetAllInventoryItems end point
@@ -19,10 +29,65 @@ export const Route = createFileRoute("/app/inventory")({
  * - Refer to get-user.ts to see an example of how we get the user with a custom hook. 
  */
 
-function Inventory() { 
+function Inventory() {
+  const businessUuid = "2b708a94-19d6-402b-b9c4-e2fef0e3f5aa"; //change this to the second business Uuid once I make it 
+  const userStore = useUserStore();
+  const userUuid = userStore.uuid;
+  const getAllInventoryItemsQuery = useGetAllInventoryItems({ userUuid, businessUuid })
+
+  const inventoryItems = getAllInventoryItemsQuery.data?.map(items => {
     return (
-      <>
-              <h1>Welcome to inventory</h1>
-      </>
+      <ul>
+        <li>{items.inventoryItemUuid}</li>
+        <li>{items.name}</li>
+        <li>{items.description}</li>
+        <li>{items.sku}</li>
+        <li>{items.cost}</li>
+        <li>{items.serialNumber}</li>
+        <li>{items.purchaseDate?.toDateString()}</li>
+        <li>{items.supplier}</li>
+        <li>{items.brand}</li>
+        <li>{items.model}</li>
+        <li>{items.quantity}</li>
+        <li>{items.reorderQuantity}</li>
+        <li>{items.location}</li>
+        <li>{items.experationDate?.toDateString()}</li>
+        <li>{items.category}</li>
+        <li>{items.customPackageUuid}</li>
+        <li>{items.itemWeightG}</li>
+        <li>{items.isListed}</li>
+        <li>{items.isLot}</li>
+        <li>{items.notes}</li>
+      </ul>
     )
+  }
+  )
+
+  if (!getAllInventoryItemsQuery.isLoading && getAllInventoryItemsQuery.data) {
+    return (
+      <div className="p-4">
+        <h1>Welcome to inventory</h1>
+        <div>{inventoryItems}</div>
+      </div>
+    )
+  };
+
+  if (!getAllInventoryItemsQuery.isLoading && !getAllInventoryItemsQuery.data) {
+    return (
+      <div className="p-4">
+        <h1>Welcome to inventory</h1>
+        <div>
+          <p>It looks like you don't have any inventory items. Would you like to make one?</p>
+        </div>
+      </div>
+    )
+  };
+
+  if (getAllInventoryItemsQuery.isLoading) {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <Spinner size="xl" />
+      </div>
+    );
+  }
 }
